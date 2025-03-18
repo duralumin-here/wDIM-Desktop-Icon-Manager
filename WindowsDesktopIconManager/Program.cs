9b437606cab4dc90f8f8dc9b3c320a25b7724bcd
duralumin-here <202533636+duralumin-here@users.noreply.github.com>
2025-03-18 16:14:51 -0400

    Finished the desktop validation -> file movement -> shortcut creation path. Not too concerned about validating entry as this will be better implemented in the eventual Windows Form application.

--- WindowsDesktopIconManager/Program.cs
+++ WindowsDesktopIconManager/Program.cs
@@ -34,102 +34,105 @@ Possible features:
 using System;
 using System.ComponentModel;
 using IWshRuntimeLibrary;
+using File = System.IO.File;
 
 namespace WindowsDesktopIconManager
 {
     internal class Program
     {
+        // This method is my main.
         static void Main(string[] args)
         {
-            Console.WriteLine("What would you like to do?\n-----\n1: Create a shortcut\n2: Validate desktop files\nOther: Leave program");
-            string userInput = Console.ReadLine();
-            if (int.TryParse(userInput, out int userEntry))
+            // String to hold non-shortcuts
+            string[] notShortcuts = new string[500];
+
+            // Count invalid files (also adds them to the non-shortcut array)
+            int invalidFileNum = ValidateDesktopFiles(notShortcuts);
+
+            if (invalidFileNum != 0)
             {
-                switch (userEntry)
+                Console.WriteLine("The following files on your desktop are not shortcuts. Please follow the prompts to fix this.\n");
+                for (int i = 0; i < invalidFileNum; ++i)
                 {
-                    case 1:
-                        CreateShortcut();
-                        break;
-                    case 2:
-                        ValidateDesktopFiles();
-                        break;
-                    default:
-                        break;
+                    Console.WriteLine(notShortcuts[i]);
+                }
+                Console.WriteLine("");
+                for (int i = 0; i < invalidFileNum; ++i)
+                {
+                    CreateShortcut(notShortcuts[i]);
+                    Console.WriteLine("");
                 }
             }
             else
             {
-                Console.WriteLine("Invalid input. Please enter a number.");
+                Console.WriteLine("All desktop files are shortcuts.");
             }
 
-        }
+            // Refresh desktop afterwards
 
-        static void CreateShortcut() // shoutout to Rustam Irzaev for the example I referenced https://stackoverflow.com/a/4909475
+
+        } // end method Main()
+
+        // This method creates a shortcut for a file based on its location
+        static void CreateShortcut(string itemPath) // shoutout to Rustam Irzaev for the example I referenced https://stackoverflow.com/a/4909475
         {
-            Console.WriteLine("Please enter the name for the shortcut.");
-            string shortcutName = Console.ReadLine(); // will need to validate that it doesn't contain illegal characters
-            Console.WriteLine("Please enter the description you want to have.");
-            string shortcutDesc = Console.ReadLine();
-            Console.WriteLine("Please enter the file path for the shortcut.");
-            string shortcutPathTo = Console.ReadLine();
+            Console.WriteLine("Please enter the path to the folder you would like to move the item to."); // FIXME: use FolderBrowserDialog once this is converted to a Windows Form
+            string newFolder = Console.ReadLine();
 
+            Console.WriteLine("Please enter a name for the shortcut (no extension needed):");
+            string shortcutName = Console.ReadLine(); // FIXME: will need to validate that it doesn't contain illegal characters
+
+            string extension = itemPath.Substring((itemPath.IndexOf(".")), (itemPath.Length - (itemPath.IndexOf("."))));
+
+            string newPath = newFolder + shortcutName + extension;
+
+            File.Move(itemPath, newPath); // Move it to a new location
             object shDesktop = (object)"Desktop";
             WshShell shell = new WshShell();
             string shortcutAddress = (string)shell.SpecialFolders.Item(ref shDesktop) + @"\" + shortcutName + ".lnk";
             IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(shortcutAddress);
-            shortcut.Description = shortcutDesc;
+            // shortcut.Description = shortcutDesc;
             // shortcut.Hotkey = "Ctrl+Shift+N";
-            shortcut.TargetPath = shortcutPathTo;
+            shortcut.TargetPath = newPath;
             shortcut.Save();
-        }
+        } // end method CreateShortcut(location)
 
-        static string CheckForQuotes(string checkedString) // doesn't seem needed anymore, but keeping for later formatting
+        // This method runs through the desktop files and finds which, if any, are not shortcuts.
+        static int ValidateDesktopFiles(string[] notShortcuts) // shoutout to https://www.csharp.com/article/directories-in-c-sharp/ for helping me learn how to do operations with files
         {
-            int quoteCheck = checkedString.IndexOf("\"");
-            while (quoteCheck != -1)
-            {
-                checkedString = checkedString.Remove(quoteCheck, 1);
-                quoteCheck = checkedString.IndexOf("\"");
-            }
-            return checkedString;
-        }
+            int numNonShortcuts = 0; // Creating it here; importing from Main doesn't help as that creates a copy of the variable and doesn't update the original address 
 
-        static void ValidateDesktopFiles() // shoutout to https://www.csharp.com/article/directories-in-c-sharp/ for helping me learn how to do operations with files
-        {
+            // Find desktop locations
             string publicDesk = @"C:\Users\Public\Desktop";
             string userDesk = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
 
-            Boolean containsNonShortcuts = false;
-
+            // Create lists of files in each desktop
             string[] publicEntries = Directory.GetFiles(publicDesk);
             string[] userEntries = Directory.GetFiles(userDesk);
-            string[] notShortcuts = new string[500];
-            int numNonShortcuts = 0;
 
-            foreach (string fileName in publicEntries)
+            // Combine both desktops into one array
+            string[] allEntries = new string[publicEntries.Length + userEntries.Length];
+            for (int i = 0; i < publicEntries.Length; ++i)
             {
-                int numCheck = fileName.IndexOf(".lnk");
-                if ((numCheck == -1) && (fileName.IndexOf("desktop.ini") == -1))
-                {
-                    containsNonShortcuts = true;
-                    notShortcuts[numNonShortcuts] = fileName;
-                    ++numNonShortcuts;
-                }
-            }
-            foreach (string fileName in userEntries)
-            {
-                int numCheck = fileName.IndexOf(".lnk");
-                if ((numCheck == -1) && (fileName.IndexOf("desktop.ini") == -1))
-                {
-                    containsNonShortcuts = true;
-                    Console.WriteLine(fileName);
-                }
+                allEntries[i] = publicEntries[i];
             }
 
-            if (!containsNonShortcuts)
+            for (int i = 0; i < userEntries.Length; ++i)
             {
-                Console.WriteLine("All desktop files are shortcuts.");
+                allEntries[publicEntries.Length + i] = userEntries[i];
             }
-        }
-    }
-}
+
+            // Iterate through desktop to find non-shortcuts
+            foreach (string fileName in allEntries)
+            {
+                int numCheck = fileName.IndexOf(".lnk"); // Find index of the shortcut file extension
+                if ((numCheck == -1) && (fileName.IndexOf("desktop.ini") == -1)) // If the shortcut extension is not present and it's not desktop.lnk
+                {
+                    notShortcuts[numNonShortcuts] = fileName; // Add file to array of non-shortcuts
+                    ++numNonShortcuts; // Increment number of non-shortcuts to track oversized array
+                }
+            }
+            return numNonShortcuts; // This originally returned a boolean for whether or not shortcuts were present. I realized I can find this by checking if the number of non-shortcuts > 0, and returning the length of the oversized array is important, so I'm changing it.
+        }  // end method ValidateDesktopFiles()
+    } // end class Program
+} // end namespace WindowsDesktopIconManager
